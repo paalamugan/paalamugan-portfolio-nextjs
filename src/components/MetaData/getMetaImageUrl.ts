e02ee59c14b715812d33b12ad0cdc263f14a663d
paalamugan <paalamugan@gmail.com>
2022-08-12 14:41:32 +0530

    chore: update profile logo

--- src/components/MetaData/getMetaImageUrl.ts
+++ src/components/MetaData/getMetaImageUrl.ts
@@ -1,7 +1,7 @@
 import { resolve } from "@app/utils";
 
 export const getMetaImageUrl = (siteUrl: string) => {
-  return resolve(siteUrl, "/images/logo.png");
+  return resolve(siteUrl, "/images/profile.png");
 };
 
 export default getMetaImageUrl;
